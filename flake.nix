ca9dce0c8a457b82b9e70be08e3f495e05a121e0
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-06-06 08:14:17 +0300

    [TMP] Now with flake
    Change-Id: I58a3f3f1783e3f6ddfd2d34347e5e96c395fb093

--- flake.nix
+++ flake.nix
@@ -1,27 +1,54 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    #pkgs-cross = import pkgs.path {
-    pkgs-cross = import <nixpkgs> {
-      crossSystem = pkgs.lib.systems.examples.armhf-embedded // {
-        rustc.config = "thumbv7em-none-eabi";
-      };
-    };
-  in
-   {
-    hello = with pkgs-cross;rustPlatform.buildRustPackage {
-      pname = "hello";
-      version = "0.1.0";
-      cargoLock = {
-        lockFile = ./Cargo.lock;
-      };
-      src = ./.;
-    };
-    np = nixpkgs;
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (localSystem:
+      let
+        pkgs = nixpkgs.legacyPackages.${localSystem};
+        crossPkgs = import nixpkgs {
+          inherit localSystem;
+          crossSystem =
+            {
+              config = "arm-none-eabi";
+              rustc.config = "thumbv7em-none-eabi";
+              #rustc.platform = {
+              #  arch = "arm";
+              #  abi = "eabi";
+              #  c-enum-min-bits = 8;
+              #  data-layout = "e-m:e-p:32:32-Fi8-i64:64-v128:64:128-a:0:32-n32-S64";
+              #  emit-debug-gdb-scripts = false;
+              #  frame-pointer = "always";
+              #  is-builtin = true;
+              #  linker = "rust-lld";
+              #  linker-flavor = "ld.lld";
+              #  llvm-target = "thumbv7em-none-eabi";
+              #  max-atomic-width = 32;
+              #  panic-strategy = "abort";
+              #  relocation-model = "static";
+              #  target-pointer-width = "32";
+              #};
+            };
+        };
+        hello_rust = crossPkgs.rustPlatform.buildRustPackage {
+          pname = "hello_rust";
+          version = "0.1.0";
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          doCheck = false;
+          #nativeBuildInputs = [ pkgs.rustup pkgs.cargo-binutils ];
+          src = ./.;
+          meta.platforms = [
+            "arm-none"
+            "armhf-none"
+          ];
+        };
+      in
+      {
+        packages.default = hello_rust;
+      }
+    );
 }
