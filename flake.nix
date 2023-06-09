27302022eca61e883ba84010b845bf530c09cef4
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-06-09 23:51:02 +0300

    Ship nix building to nix-stm32 framework
    Change-Id: Ic8a25d94b0288d0350964a6b0825fe427d5abd21

--- flake.nix
+++ flake.nix
@@ -1,64 +1,16 @@
 {
   description = "The rust hello project for stm32";
-
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
+    stm32.url = github:ein-shved/nix-stm32;
   };
-
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (localSystem:
-      let
-        target = "thumbv7m-none-eabi";
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit localSystem overlays;
-        };
-        rustVersion = pkgs.rust-bin.stable.latest.default.override {
-          targets = [ target ];
-        };
-
-        hello_rust = pkgs.rustPlatform.buildRustPackage {
-          pname = "hello_rust";
-          version = "0.1.0";
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-          doCheck = false;
-          src = ./.;
-          nativeBuildInputs = with pkgs; [
-            rustVersion
-            probe-run
-          ];
-          buildPhase = ''
-            runHook preBuild
-            ${rustVersion}/bin/cargo build -j $NIX_BUILD_CORES --frozen
-            runHook postBuild
-          '';
-          dontCargoInstall = true;
-          installPhase = ''
-            mkdir -p $out/bin
-            ${rustVersion}/bin/cargo install -j $NIX_BUILD_CORES --root $out --path .
-          '';
-        };
-        runner = pkgs.writeShellScriptBin "hello_rust" ''
-          ${pkgs.probe-run}/bin/probe-run --chip STM32F103C8 \
-            $(find ${hello_rust} -type f -executable)
-        '';
-      in
-      {
-        devShells.default = pkgs.mkShellNoCC {
-          buildInputs = with pkgs; [
-            rustVersion
-            probe-run
-          ];
-        };
-        packages = {
-          default = hello_rust;
-          inherit (pkgs) rust-bin;
-        };
-        apps.default = flake-utils.lib.mkApp { drv = runner; };
-      }
-    );
+  outputs = { self, stm32, nixpkgs } :
+  stm32.mkRustFirmware {
+    pname = "hello_rust";
+    version = "0.1.0";
+    mcu = stm32.mcus.stm32f103;
+    src = ./.;
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+    };
+  };
 }
