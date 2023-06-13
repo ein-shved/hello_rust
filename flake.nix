2f5c54f1303ad827035eed577b70b624a27d1581
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-06-13 20:02:47 +0300

    Move generic hello project to separate package
    This is preparation for adding an embassy hello project
    
    Change-Id: Ie1223b38bcbdf9dfb9b25e11db9433e493e5a7cf

--- flake.nix
+++ flake.nix
@@ -4,13 +4,19 @@
     stm32.url = github:ein-shved/nix-stm32;
   };
   outputs = { self, stm32, nixpkgs } :
-  stm32.mkRustFirmware {
-    pname = "hello_rust";
-    version = "0.1.0";
-    mcu = stm32.mcus.stm32f103;
-    src = ./.;
-    cargoLock = {
-      lockFile = ./Cargo.lock;
+  let
+    generic = stm32.mkRustFirmware {
+      pname = "hello_rust";
+      version = "0.1.0";
+      mcu = stm32.mcus.stm32f103;
+      src = ./generic/.;
+      cargoLock = {
+        lockFile = ./generic/Cargo.lock;
+      };
     };
+  in
+  stm32.mergeFirmwares {
+    inherit generic;
+    default = generic;
   };
 }
