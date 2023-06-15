d442089a0952abb36928bbb889881315d7f6bc27
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-06-15 21:13:04 +0300

    Add embassy-based hello project
    Change-Id: I560c4c1b513d5730ddfe1972b0374a43b0593944

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "The rust hello project for stm32";
   inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
     stm32.url = github:ein-shved/nix-stm32;
+    stm32.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, stm32, nixpkgs } :
   let
@@ -14,9 +16,25 @@
         lockFile = ./generic/Cargo.lock;
       };
     };
+    embassy = stm32.mkRustFirmware {
+      pname = "hello_embassy";
+      version = "0.1.0";
+      mcu = stm32.mcus.stm32f103;
+      nightly = true;
+      buildPackage = false; # TODO (Shvedov) failed to build embassy libs
+                            # with nix as dependency to this package
+      src = ./embassy/.;
+      cargoLock = {
+        lockFile = ./embassy/Cargo.lock;
+        outputHashes = {
+          "embassy-embedded-hal-0.1.0" =
+            "sha256-zBxLMNGqspi2dWvbVq1b50MWlkrdUhuv52L1w/pc3+w=";
+        };
+      };
+    };
   in
   stm32.mergeFirmwares {
-    inherit generic;
-    default = generic;
+    inherit generic embassy;
+    default = embassy;
   };
 }
